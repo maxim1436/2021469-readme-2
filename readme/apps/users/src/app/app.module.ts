14927c2cfcbcbe76ef38cbc7cf605815ed2c8537
maxim1436 <pivanov_m@mail.ru>
2023-01-07 00:04:07 +1000

    создаём базу данных

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from '../config/database.config';
+import {validateEnvironments} from './env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoDbConfig } from '../config/mongodb.config';
 
 @Module({
-  imports: [BlogUserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+      validate: validateEnvironments,
+    }),
+    MongooseModule.forRootAsync(
+      getMongoDbConfig()
+    ),
+    AuthModule,
+    BlogUserModule
+  ],
   controllers: [],
   providers: [],
 })
